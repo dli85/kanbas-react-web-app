624d1ab04989b6bb36b95c50f0cdc4edd4a0b935
David Li <dali7758769@gmail.com>
2024-03-17 01:58:05 -0400

    3.2 done

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,37 +1,48 @@
 import { courses } from "../../Kanbas/Database";
-import { useParams, Navigate, Route, Routes, useLocation} from "react-router-dom";
+import {
+  useParams,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import { HiMiniBars3 } from "react-icons/hi2";
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 
-function Courses() {
-  const { courseId, } = useParams();
+function Courses({ courses }: { courses: any[] }) {
+  const { courseId } = useParams();
   const course = courses.find((course) => course._id === courseId);
   // useLocation is used to get the last url param for the breadcrumb
   const location = useLocation();
-  const currentPath = location.pathname.split('/').pop();
+  const currentPath = location.pathname.split("/").pop();
   return (
     <div>
-      <h1><HiMiniBars3 /> Course {course?.name} {">"} {currentPath}</h1>
+      <h1>
+        <HiMiniBars3 /> Course {course?.name} {">"} {currentPath}
+      </h1>
       <CourseNavigation />
       <div>
         <div
           className="overflow-y-scroll position-fixed bottom-0 end-0"
-          style={{ left: "320px", top: "50px" }} >
+          style={{ left: "320px", top: "50px" }}
+        >
           <Routes>
             <Route path="/" element={<Navigate to="Home" />} />
-            <Route path="Home" element={<Home/>} />
-            <Route path="Modules" element={<Modules/>} />
+            <Route path="Home" element={<Home />} />
+            <Route path="Modules" element={<Modules />} />
             <Route path="Piazza" element={<h1>Piazza</h1>} />
-            <Route path="Assignments" element={<Assignments/>} />
-            <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
+            <Route path="Assignments" element={<Assignments />} />
+            <Route
+              path="Assignments/:assignmentId"
+              element={<h1>Assignment Editor</h1>}
+            />
             <Route path="Grades" element={<h1>Grades</h1>} />
           </Routes>
         </div>
       </div>
-
     </div>
   );
 }
