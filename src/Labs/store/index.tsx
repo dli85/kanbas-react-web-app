23dd56a7aa951f3ad2b93de5319d603231d0bcb5
David Li <dali7758769@gmail.com>
2024-03-17 00:46:00 -0400

    lab done

---
+++ src/Labs/store/index.tsx
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import helloReducer from "../a4/ReduxExamples/HelloRedux/helloReducer";
+import counterReducer from "../a4/ReduxExamples/CounterReducer/counterReducer";
+import addReducer from "../a4/ReduxExamples/AddRedux/addReducer";
+import TodoReducer from "../a4/ReduxExamples/todos/TodoReducer";
+export type TodoType = {
+  id: string;
+  title: string;
+};
+
+export interface LabState {
+  helloReducer: {
+    message: string;
+  };
+  counterReducer: {
+    count: number;
+  };
+  addReducer: {
+    sum: number;
+  };
+  todosReducer: {
+    todos: TodoType[];
+    todo: TodoType;
+  };
+}
+const store = configureStore({
+  reducer: {
+    helloReducer,
+    counterReducer,
+    addReducer,
+    TodoReducer,
+  },
+});
+export default store;
